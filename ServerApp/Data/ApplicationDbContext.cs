dcdccf50da11aca66f5f056f23d0d3687df21969
Felipe Romero <feliperomero3@outlook.com>
2020-07-28 00:14:31 -0500

    Create the Database Context Class

---
+++ ServerApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ServerApp.Models;
+
+namespace ServerApp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Supplier> Suppliers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>(builder => { builder.Property(p => p.Price).HasColumnType("decimal(8, 2)"); });
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
