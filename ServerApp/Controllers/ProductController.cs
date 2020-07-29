07292d4aa8ab832503074cc029419046c7c4f2e0
Felipe Romero <feliperomero3@outlook.com>
2020-07-28 21:16:08 -0500

    Create the Web Service Controller

---
+++ ServerApp/Controllers/ProductController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ServerApp.Data;
+using ServerApp.Models;
+
+namespace ServerApp.Controllers
+{
+    [Route("api/products")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly ApplicationDbContext _applicationDbContext;
+
+        public ProductController(ApplicationDbContext applicationDbContext)
+        {
+            _applicationDbContext = applicationDbContext;
+        }
+
+        [HttpGet("{id}")]
+        public Product GetProduct(long id)
+        {
+            return _applicationDbContext.Products.Find(id);
+        }
+    }
+}
