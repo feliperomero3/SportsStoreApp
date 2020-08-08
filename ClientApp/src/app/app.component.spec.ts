e51a12e9509608c2b486b551d19a85864a3c3b6a
Felipe Romero <feliperomero3@outlook.com>
2020-08-07 20:01:59 -0500

    Add new app component tests

--- ClientApp/src/app/app.component.spec.ts
+++ ClientApp/src/app/app.component.spec.ts
@@ -10,7 +10,7 @@ describe('AppComponent', () => {
   const mockSupplierService: any = {};
 
   beforeEach(async(() => {
-    mockProductService = jasmine.createSpyObj(['getProducts']);
+    mockProductService = jasmine.createSpyObj(['getProduct', 'getProducts']);
     TestBed.configureTestingModule({
       imports: [
         RouterTestingModule
@@ -39,12 +39,34 @@ describe('AppComponent', () => {
 
   it('should render title \'Products Catalog\'', () => {
     const fixture = TestBed.createComponent(AppComponent);
-    const product = [{ name: 'Kayak', category: 'Water sports', price: '276' }];
-    mockProductService.getProducts.and.returnValue(of(product));
+    mockProductService.getProducts.and.returnValue(of([]));
 
     fixture.detectChanges();
 
     const compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelector('h2').textContent).toContain('Products Catalog');
+  });
+
+  it('can retrieve a product', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const product = { name: 'Kayak', category: 'Water sports', price: '276' };
+    mockProductService.getProduct.and.returnValue(of(product));
+
+    fixture.componentInstance.getProduct(1);
+
+    expect(fixture.componentInstance.product).toBeTruthy();
+  });
+
+  it('can retrieve multiple products', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const products = [
+      { name: 'Kayak', category: 'Water sports', price: '276' },
+      { name: 'Lifejacket', category: 'Water sports', price: '49.95' }
+    ];
+    mockProductService.getProducts.and.returnValue(of(products));
+
+    fixture.componentInstance.getProducts();
+
+    expect(fixture.componentInstance.products.length).toBe(2);
   });
 });
