da55a2ae0d4dc6d9ee3c117a68e2c941ff4540fa
Felipe Romero <feliperomero3@outlook.com>
2020-08-07 21:50:36 -0500

    Create first ServerApp Integration Test

---
+++ ServerApp.IntegrationTests/HealthCheckTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Xunit;
+
+namespace ServerApp.IntegrationTests
+{
+    public class HealthCheckTests : IClassFixture<WebApplicationFactory<Startup>>
+    {
+        private readonly HttpClient _httpClient;
+
+        public HealthCheckTests(WebApplicationFactory<Startup> factory)
+        {
+            _httpClient = factory.CreateDefaultClient(new Uri("https://localhost:5001"));
+        }
+
+        [Fact]
+        public async Task HealthCheck_Returns_Ok()
+        {
+            var response = await _httpClient.GetAsync("/healthcheck");
+
+            response.EnsureSuccessStatusCode();
+        }
+    }
+}
