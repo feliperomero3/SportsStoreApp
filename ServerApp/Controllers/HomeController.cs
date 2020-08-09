2d6a30f174a7f18385996c2719735e3ce9581e7b
Felipe Romero <feliperomero3@outlook.com>
2020-08-08 23:29:44 -0500

    Remove unused logger in HomeController

--- ServerApp/Controllers/HomeController.cs
+++ ServerApp/Controllers/HomeController.cs
@@ -1,19 +1,11 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 using ServerApp.ViewModels;
 
 namespace ServerApp.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
         public IActionResult Index()
         {
             return View();
