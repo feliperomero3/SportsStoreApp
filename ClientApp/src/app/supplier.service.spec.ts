1827c0452c86513572ed790aadcf89fa2394af1f
Felipe Romero <feliperomero3@outlook.com>
2020-08-24 16:48:20 -0500

    Add supplier service createSupplier unit test

--- ClientApp/src/app/supplier.service.spec.ts
+++ ClientApp/src/app/supplier.service.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed } from '@angular/core/testing';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { SupplierService } from './supplier.service';
+import { Supplier } from './supplier';
 
 describe('SupplierService', () => {
   beforeEach(() => TestBed.configureTestingModule({
@@ -15,5 +16,17 @@ describe('SupplierService', () => {
   it('should be created', () => {
     const service: SupplierService = TestBed.get(SupplierService);
     expect(service).toBeTruthy();
+  });
+
+  it('should set the correct URL to create a supplier', () => {
+    const service: SupplierService = TestBed.get(SupplierService);
+    const controller: HttpTestingController = TestBed.get(HttpTestingController);
+    const supplier = new Supplier(1, 'Splash Dudes', 'San Jose', 'CA');
+
+    service.createSupplier(supplier).subscribe();
+
+    const req = controller.expectOne('api/suppliers');
+    expect(req.request.url).toBe('api/suppliers');
+    controller.verify();
   });
 });
