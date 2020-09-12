6ec695616975cf25556b4c1946cc7ad1658910bf
Felipe Romero <feliperomero3@outlook.com>
2020-09-11 20:48:25 -0500

    Add product service createProduct test

--- ClientApp/src/app/product.service.spec.ts
+++ ClientApp/src/app/product.service.spec.ts
@@ -1,6 +1,8 @@
 import { TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { ProductService } from './product.service';
+import { Product } from './product';
+import { Supplier } from './supplier';
 
 describe('ProductService', () => {
   beforeEach(() => TestBed.configureTestingModule({
@@ -48,5 +50,19 @@ describe('ProductService', () => {
     const req = controller.expectOne('api/products?search=Kayak');
     controller.verify();
     expect(req.request.urlWithParams).toBe('api/products?search=Kayak');
+  });
+
+  it('should set the correct URL to create a product', () => {
+    const service: ProductService = TestBed.get(ProductService);
+    const controller: HttpTestingController = TestBed.get(HttpTestingController);
+    const supplier = new Supplier(1, 'Splash Dudes', 'San Jose', 'CA');
+    const product = new Product(0, 'Splash Snorkel', 'Silicone snorkel with semi-dry top', 'watersports',
+      14.49, supplier);
+
+    service.createProduct(product).subscribe();
+
+    const req = controller.expectOne('api/products');
+    expect(req.request.url).toBe('api/products');
+    controller.verify();
   });
 });
