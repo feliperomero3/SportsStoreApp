e2dcc0c6dab5d73f788612fd7573de0b296d5c07
Felipe Romero <feliperomero3@outlook.com>
2021-04-04 00:15:08 -0600

    Remove product service http headers property trailing comma

--- ClientApp/src/app/product.service.ts
+++ ClientApp/src/app/product.service.ts
@@ -12,7 +12,7 @@ export class ProductService {
   private url = 'api/products';
   private httpOptions = {
     headers: new HttpHeaders({
-      'Content-Type': 'application/json',
+      'Content-Type': 'application/json'
     })
   };
 
